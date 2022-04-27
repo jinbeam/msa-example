35dc48ed7ba62e1ae11bbc60504076a968f31ce8
jinsu <jinsu@ezpmp.co.kr>
2022-04-27 11:41:39 +0900

    소스 정리

---
+++ backend/menu/src/main/java/com/sample/api/MenuApplication.java
@@ -0,0 +1,14 @@
+package com.sample.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableJpaAuditing
+public class MenuApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MenuApplication.class, args);
+	}
+}
